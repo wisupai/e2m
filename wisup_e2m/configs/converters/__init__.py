55bbb8132cd7d2792b29f32f44dd54e25549428a
Jing-yilin <yilin.jing.ai@outlook.com>
2024-08-18 16:12:59 +0800

    style: format

--- wisup_e2m/configs/converters/__init__.py
+++ wisup_e2m/configs/converters/__init__.py
@@ -1,7 +1,4 @@
 from wisup_e2m.configs.converters.text_converter_config import TextConverterConfig
 from wisup_e2m.configs.converters.image_converter_config import ImageConverterConfig
 
-__all__ = [
-    "TextConverterConfig",
-    "ImageConverterConfig"
-]
+__all__ = ["TextConverterConfig", "ImageConverterConfig"]
--- wisup_e2m/configs/parsers/__init__.py
+++ wisup_e2m/configs/parsers/__init__.py
@@ -17,5 +17,5 @@ __all__ = [
     "PptxParserConfig",
     "UrlParserConfig",
     "HtmlParserConfig",
-    "VoiceParserConfig"
+    "VoiceParserConfig",
 ]
