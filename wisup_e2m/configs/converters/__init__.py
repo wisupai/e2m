00c146b01a1ec48178855ccfc83a29612bb4c9b7
Jing-yilin <yilin.jing.ai@outlook.com>
2024-08-18 16:04:26 +0800

    pref: add config __init__ file

---
+++ wisup_e2m/configs/converters/__init__.py
@@ -0,0 +1,7 @@
+from wisup_e2m.configs.converters.text_converter_config import TextConverterConfig
+from wisup_e2m.configs.converters.image_converter_config import ImageConverterConfig
+
+__all__ = [
+    "TextConverterConfig",
+    "ImageConverterConfig"
+]
---
+++ wisup_e2m/configs/parsers/__init__.py
@@ -0,0 +1,21 @@
+from wisup_e2m.configs.parsers.doc_parser_config import DocParserConfig
+from wisup_e2m.configs.parsers.docx_parser_config import DocxParserConfig
+from wisup_e2m.configs.parsers.pdf_parser_config import PdfParserConfig
+from wisup_e2m.configs.parsers.epub_parser_config import EpubParserConfig
+from wisup_e2m.configs.parsers.ppt_parser_config import PptParserConfig
+from wisup_e2m.configs.parsers.pptx_parser_config import PptxParserConfig
+from wisup_e2m.configs.parsers.url_parser_config import UrlParserConfig
+from wisup_e2m.configs.parsers.html_parser_config import HtmlParserConfig
+from wisup_e2m.configs.parsers.voice_parser_config import VoiceParserConfig
+
+__all__ = [
+    "DocParserConfig",
+    "DocxParserConfig",
+    "PdfParserConfig",
+    "EpubParserConfig",
+    "PptParserConfig",
+    "PptxParserConfig",
+    "UrlParserConfig",
+    "HtmlParserConfig",
+    "VoiceParserConfig"
+]
