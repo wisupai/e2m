0ab5fa912a73c9beae5df7503bdfacad0f95a593
Jing-yilin <yilin.jing.ai@outlook.com>
2024-08-18 16:32:15 +0800

    🐞 fix: BaseConverterConfig bug

--- wisup_e2m/configs/converters/text_converter_config.py
+++ wisup_e2m/configs/converters/text_converter_config.py
@@ -1,6 +1,6 @@
-from wisup_e2m.configs.converters.base import BaseConverter
+from wisup_e2m.configs.converters.base import BaseConverterConfig
 
 
-class TextConverterConfig(BaseConverter):
+class TextConverterConfig(BaseConverterConfig):
 
     pass
